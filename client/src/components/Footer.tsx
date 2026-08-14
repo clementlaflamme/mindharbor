deae1556774dc374a1aad4140fad23f17b68b399
Mathieu Gosselin <math.gosselin@gmail.com>
2026-08-14 15:21:29 -0400

    feature: fondation pour le front-end

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+        <div className="badgeArea">
+            <div className="badge911">911</div>
+            <div className="badge866">1-866-APPELLE</div>
+        </div>
+        <p>&copy; 2026 MindHarbor. Tous droits réservés.</p>
+    
+    </footer>
+  );
+}
