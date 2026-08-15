aafe961310f9382af59c3c37209e5be413ad8d27
Clement Laflamme <clement_laflamme@videotron.ca>
2026-08-14 21:13:28 -0400

    fix: unused import

--- client/src/components/Inscription.tsx
+++ client/src/components/Inscription.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import "./css/formulaire.css";
-import { useAuth } from "../api/context/AuthContext";
 import { api } from "../api/api";
 import axios from "axios";
 
