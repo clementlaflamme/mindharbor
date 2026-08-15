6b94af0efc01c3e56a43e6784db65d94cb632617
powemetal <francisbvrt@gmail.com>
2026-08-14 20:36:24 -0400

    Debut du squelette du module de la messagerie

--- client/src/components/Messagerie.tsx
+++ client/src/components/Messagerie.tsx
@@ -1,7 +1,46 @@
+import "./messagerie.css";
+
+
 export default function Messagerie() {
   return (
-    <div className="container-messagerie">
-        <h2>Écran Messagerie</h2>
+        
+    <div className="messagerie-page">
+      <h2 className="titre">Messagerie</h2>
+      
+        <div className="messagerie-layout">
+
+
+          <aside className="messagerie-sidebar">
+            <h3>Conversations</h3>
+            <ul>
+              <li><p></p></li>
+              <li><p>conv</p></li>
+              <li><p>conv</p></li>
+            </ul>
+            <button className="new-message-btn">Nouveau Message</button>
+          </aside>
+
+
+
+          <main className="messagerie-conversation">
+            <h3>Messages</h3>
+            <ul>
+              <li><p>message</p></li>
+              <li><p>message</p></li>
+              <li><p>message</p></li>
+            </ul>
+            <form className="message-input-container">
+              <input className="message-input" placeholder="Entrez votre message ici" type="text"></input>
+              <button className="message-send-btn">Envoyer</button>
+            </form>
+            
+          </main>
+
+
+
+  
+
+      </div>
     </div>
   );
 }
